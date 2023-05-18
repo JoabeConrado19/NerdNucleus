68b30f95d4b06be5be750e182ac7ba3f93e89ad1
JoabeConrado19 <joabe.conrado19@gmail.com>
2023-05-18 17:05:47 -0400

    fix api link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nerdnucleusapi-production.up.railway.app/api",
+  baseURL: "https://nerdnucleusapidjango.onrender.com/api",
   timeout: 10000,
   
 });
